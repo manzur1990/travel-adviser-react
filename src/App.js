763f8253c0c58376cb787af84b2c6f62e74a72fb
manzur1990 <manzur-1990@hotmail.com>
2022-08-15 21:21:35 -0600

    adding styles from material ui core

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
-    return <div>Hello, World</div>;
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <List />
+            <Map />
+        </>
+    );
 };
 
 export default App;
