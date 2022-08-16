ba89c9c244ef1b99c2c814cda05a206401286af3
manzur1990 <manzur-1990@hotmail.com>
2022-08-15 22:11:41 -0600

    fixed wrong location on inputbase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Box, InputBase } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './HeaderStyles';
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <Toolbar className={classes.toolbar}>
-            <Typography variant='h5' classname={classes.title}>
-                Travel Advisor
-            </Typography>
-            <Box display='flex'>
-                <Typography variant='h6' classname={classes.title}>
-                    Explore new places
+        <AppBar position='static'>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' classname={classes.title}>
+                    Travel Advisor
                 </Typography>
-                <Autocomplete>
+                <Box display='flex'>
+                    <Typography variant='h6' classname={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <Autocomplete> */}
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon />
-                            <InputBase
-                                placeholder='Search... '
-                                classes={{
-                                    root: classes.inputRoot,
-                                    input: classes.inputInput,
-                                }}
-                            />
                         </div>
+                        <InputBase
+                            placeholder='Search... '
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
                     </div>
-                </Autocomplete>
-            </Box>
-        </Toolbar>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
 export default Header;
