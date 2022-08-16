408c2c18baec9dc5b34b84ca91f5173542738ddf
manzur1990 <manzur-1990@hotmail.com>
2022-08-15 21:24:29 -0600

    added initial layout

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ const App = () => {
         <>
             <CssBaseline />
             <Header />
-            <List />
-            <Map />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
         </>
     );
 };
