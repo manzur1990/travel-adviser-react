7e7bfe654edcd11a26001e447b4f54e54c8a348c
manzur1990 <manzur-1990@hotmail.com>
2022-08-17 11:47:15 -0600

    fixed typo

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,23 +1,24 @@
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
 const Map = () => {
+    const matches = useMediaQuery('(min-width:600px)');
     const classes = useStyles();
-    const isMobile = useMediaQuery('min-width:600px');
+    const coords = { lat: 0, lng: 0 };
 
-    const coordinates = { lat: 0, lng: 0 };
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
                 bootstrapURLKeys={{
                     key: 'AIzaSyB3adT4oON5OwDcgibu_ADSAoICKjjb4J4',
                 }}
-                defualtCenter={coordinates}
-                cetner={coordinates}
+                defau
+                ltCenter={coords}
+                center={coords}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={''}
@@ -26,4 +27,5 @@ const Map = () => {
         </div>
     );
 };
+
 export default Map;
