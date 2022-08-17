3f5b83685f74f28f5f133febe892285870f95414
manzur1990 <manzur-1990@hotmail.com>
2022-08-16 22:17:10 -0600

    added UI elements to map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,29 @@
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
 const Map = () => {
-    return <h1>Map</h1>;
+    const classes = useStyles();
+    const isMobile = useMediaQuery('min-width:600px');
+
+    const coordinates = { lat: 0, lng: 0 };
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: 'AIzaSyB3adT4oON5OwDcgibu_ADSAoICKjjb4J4',
+                }}
+                defualtCenter={coordinates}
+                cetner={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}></GoogleMapReact>
+        </div>
+    );
 };
 export default Map;
