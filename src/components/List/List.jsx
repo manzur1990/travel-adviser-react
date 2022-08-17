eb987aedef210cc798a3e4e73d14bf9170d547ae
manzur1990 <manzur-1990@hotmail.com>
2022-08-17 11:49:39 -0600

    added list styles

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,15 @@
+import {
+    CircularProgress,
+    Grid,
+    Typography,
+    InputLabel,
+    MenuItem,
+    FromContol,
+    Select,
+} from '@material-ui/core';
+
+import useStyles from './styles';
+
 const List = () => {
     return <h1>List</h1>;
 };
